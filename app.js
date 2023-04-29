954e36c9c39f76393e4ef9e0633837335331e35a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-29 12:36:10 +0200

    home : got time successfully

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-let div_dday = document.querySelector('.div-dday')
-let div_dday_fmt_num = document.querySelector('.div-dday-fmt h4')
+//let counter = document.querySelector('.div-counter')
+//let counter_fmt = document.querySelector('.div-counter-fmt h4')
+// next doomsday:
+//  May 9, 2O23
+//   O-index month : May - 4
+//   timezone hour : 7am -> 9am
+let target = new Date(2023, 4, 9, 9, 59, 0)
+console.log(target)
+let year = target.getFullYear()
