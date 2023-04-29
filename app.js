e1326be1890b9f341a86eb201c89094da9ff1063
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-29 19:17:51 +0200

    figuring out

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //let counter = document.querySelector('.div-counter')
 //let counter_fmt = document.querySelector('.div-counter-fmt h4')
+
+let toPrint = document.querySelector(".article-explanatory-notes")
+
 // next doomsday:
 //  May 9, 2O23
 //   O-index month : May - 4
@@ -7,3 +10,9 @@
 let target = new Date(2023, 4, 9, 9, 59, 0)
 console.log(target)
 let year = target.getFullYear()
+let hours = target.getHours()
+let mins = target.getMinutes()
+
+let 
+
+toPrint.textContent = `how much time left to a specified date: ${hours} ${minutes}`
